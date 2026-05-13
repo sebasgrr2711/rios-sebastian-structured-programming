1ae9e495134917c6b1bdce20b86e2fd60c4b35e8
sebasgrr2711 <sebastian_3171250004@utd.edu.mx>
2026-05-12 23:42:04 -0600

    Add files via upload

---
+++ UNIT1/Lab2_Rios_Sebastian.py
@@ -0,0 +1,24 @@
+#CODE 1
+def multiply(a,b):
+    return a*b
+print (multiply(3,4))
+
+#CODE 2
+counter=0
+
+for i in range (4):
+    counter=counter+1
+
+#CODE 3 Add ":" at the end of the if
+age= int(input("Enter your age: "))
+if age>=18:
+    print("Adult")
+
+#CODE 4
+a=3
+b=4
+temp=a
+a=b
+b=temp
+print(a)
+print(b)
